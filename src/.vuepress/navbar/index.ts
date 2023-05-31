7649b8ba7cdeaa2a76d4c3dd904157c1ae1a34b3
SherkeyXD <253294679@qq.com>
2023-05-31 22:44:26 +0800

    feat: Vuepress-Hope 主题初始化

---
+++ src/.vuepress/navbar/index.ts
@@ -0,0 +1,14 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const Navbar = navbar([
+  { 
+    text: "首页", 
+    icon: "home", 
+    link: "/" 
+  },
+  { 
+    text: "题库", 
+    icon: "blog", 
+    link: "/banks/" 
+  },
+]);
