e314e4daca4c72013be012159d2f5d723a801f96
F5 <i@f5soft.site>
2023-06-08 13:36:46 +0800

    Attempt to change the base url

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,21 +3,19 @@ import theme from "./theme.js";
 
 
 export default defineUserConfig({
-  base: "/",
+  base: "/RubbishMaoGai/",
 
   locales: {
     "/": {
       lang: "zh-CN",
       title: "厦门大学 Course 题库",
-      description: "一个收集厦门大学course.xmu.edu.cn题库的网站",
+      description: "一个收集厦门大学 course.xmu.edu.cn 题库的网站",
     },
   },
 
   theme,
 
-  plugins: [
-    
-  ],
+  plugins: [],
 
   // Enable it with pwa
   // shouldPrefetch: false,
