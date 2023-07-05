259ae36d46168368d5ee55d988d528582b2a32ca
SherkeyXD <253294679@qq.com>
2023-07-05 08:17:49 +0800

    perf: 使用BeautifulSoup进行页面解析
    fix: 修复偶然出现的html标签问题

--- tiku.py
+++ tiku.py
@@ -1,23 +1,24 @@
-import os, sys
-import argparse
+from bs4 import BeautifulSoup as bs
+from argparse import ArgumentParser
+import os
 import html
 import re
 
-parser = argparse.ArgumentParser(description='根据传入的科目名生成对应的题库')
+parser = ArgumentParser(description='根据传入的科目名生成对应的题库')
 parser.add_argument('subject', type=str, help='科目名称拼音首字母  mg: 毛概 | sg: 史纲 | my: 马原')
 args = parser.parse_args()
+
 
 def unescape(raw: str):
     """
     转换十进制Unicode表示的字符
     """
-    ans = raw.strip()
-    ans = html.unescape(html.unescape(ans))
+    ans = html.unescape(raw)
+    ans = ans.strip()
     ans = ans.replace("<p>", '').replace("</p>", '')
     ans = ans.replace("<br>", '').replace(' ', '').lstrip('>')
     # ans = ans.replace("（", '(').replace("）", ')')
     return ans
-
 
 
 def getSamplesInfo(subject : str):
@@ -32,15 +33,11 @@ def getSamplesInfo(subject : str):
         "my": "马原"
     }
 
-    prefix = "---\neditLink: false\n---\n# " + subject_name[subject] + "题库\n\n::: info\n全文搜索时，由于标点符号全半角、空格数量不匹配等问题，不建议复制整个标题搜索，可能会找不到结果！建议仅搜索题目标题的连续文字内容\n:::\n\n## 题目列表\n\n"
+    prefix = "---\neditLink: false\n---\n# " + subject_name[subject] + "题库\n\n::: info\n全文搜索时，由于标点符号全半角、空格数量不匹配等问题，不建议复制整个标题搜索，可能会找不到结果！建议仅搜索题目标题的连续文字内容\n:::\n\n## 题目列表\n"
 
-    questions = set()
+    questions = []
     question_count = 0
-    question_count_list = []
     total_count = 0
-    total_count_list = []
-    choices = []
-    answers = []
 
     data_path = subjects_path[subject]
     tiku_path = os.path.join("src/banks", subject+".md")
@@ -48,7 +45,7 @@ def getSamplesInfo(subject : str):
     if not os.path.exists("./src/banks"):
         os.mkdir("./src/banks")
 
-    with open(tiku_path, 'wt', encoding="utf8", errors="replace") as tiku:
+    with open(tiku_path, 'wt', encoding="UTF-8", errors="replace") as tiku:
         tiku.write(prefix)
     
         for dirname in os.listdir(data_path):
@@ -57,59 +54,34 @@ def getSamplesInfo(subject : str):
                     continue
                 
                 filename = os.path.join(data_path, dirname, filename)
-                with open(filename, 'rt', encoding='GBK') as f:
-                    in_context = False
-                    in_answer_context = False
+                soup = bs(open(filename, "r", encoding="GBK"), "html.parser")
 
-                    for line in f.readlines():
-                        # 题目标题
-                        if "_content" in line:
+                tables = soup.find_all("table", attrs={"class": "infotable"})
+                for i in tables:
+                    title = i.find("input", attrs={"name": re.compile("[0-9]*_content")})
+                    if title:
+                        title = unescape(title["value"])
+                        if re.search(r'<.*?>(.*?)</.*?>', title):
+                            title = re.search(r'<.*?>(.*?)</.*?>', title).group(1)
+                        if title in questions:
                             total_count += 1
-                            low = line.find('value=') + 7
-                            high = line.find('><iframe') - 1
-                            contents = re.findall('[\u4e00-\u9fa5A-Za-z0-9（）\(\)《》“”"",\.，。：；]+', unescape(line[low:high]))
-                            title = ''
-                            for i in contents:
-                                if i != '宋体' and i != '黑体' and i != '微软雅黑':
-                                    title += i
-
-                            if title not in questions:
-                                in_context = True
-                                questions.add(title)
-                                question_count += 1
-                                tiku.write("### " + title + " \n\n")
-
-                            question_count_list.append(question_count)
-                            total_count_list.append(total_count)
-
-                        # 题目选项
-                        if in_context and "answer" in line and "sogoutip" not in line:
-                            low = line.find('answer') + 8
-                            choices.append(unescape(line[low:]))
-
-                        # 题目答案区域开始
-                        if in_context and "[参考答案]" in line:
-                            in_answer_context = True
                             continue
-                        
-                        # 题目答案选项内容（有多选情况）
-                        if in_answer_context:
-                            for i in re.findall('\S+', unescape(line)):
-                                if i != "</td>" and i != "":
-                                    answers.append(i)
-
-                        # 题目答案区域结束
-                        if in_answer_context and "</td>" in line:
-                            in_context = False
-                            in_answer_context = False
-                            for i in choices:
-                                if i in answers:
-                                    tiku.write("- [x] " + i + "\n")
-                                else:
-                                    tiku.write("- [ ] " + i + "\n")
-                            tiku.write("\n")
-                            choices.clear()
-                            answers.clear()
+                        else:
+                            questions.append(title)
+                            question_count += 1
+                            total_count += 1
+                            choices = i.find_all("input", attrs={"name": "answer"})
+                            if choices:
+                                choices = [j.next_sibling.replace("\n", "").strip() for j in choices]
+                                answers = i.find_all("td", attrs={"colspan":"2", "class": "Fimg"})
+                                if answers:
+                                    answers = [k.strip() for k in [j.get_text(strip=False).split("\n") for j in answers if "[参考答案]" in j.text][0] if k.strip() != ""][1:]
+                                    tiku.write(f"\n### {title}\n\n")
+                                    for j in choices:
+                                        if j in answers:
+                                            tiku.write(f"- [x] {j}\n")
+                                        else:
+                                            tiku.write(f"- [] {j}\n")     
 
     with open("./src/banks/README.md", "a", encoding="utf8") as f:
         f.write(f"| [{subject_name[subject]}](/RubbishMaoGai/banks/{subject}.html) | {total_count} | {question_count} |\n")
